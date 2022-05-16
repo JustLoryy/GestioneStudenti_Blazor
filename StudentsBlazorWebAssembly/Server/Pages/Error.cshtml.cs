d7bbef344b41e77369001bd5e73f399cda3d9702
JustLoryy <lorenzo.ribel.battistoni@gmail.com>
2022-05-16 12:18:54 +0200

    dimostrazione git

--- StudentsBlazorWebAssembly/Server/Pages/Error.cshtml.cs
+++ StudentsBlazorWebAssembly/Server/Pages/Error.cshtml.cs
@@ -6,6 +6,8 @@ namespace StudentsBlazorWebAssembly.Server.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
+
+    //test
     public class ErrorModel : PageModel
     {
         public string? RequestId { get; set; }
