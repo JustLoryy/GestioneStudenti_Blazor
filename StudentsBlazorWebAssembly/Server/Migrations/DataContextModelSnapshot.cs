52ca81fe9891c7f9e1d087267787b298782174b7
JustLoryy <lorenzo.ribel.battistoni@gmail.com>
2022-06-18 02:24:02 +0200

    Changed name to the project && fixed delete student page loading

--- StudentsBlazorWebAssembly/Server/Migrations/DataContextModelSnapshot.cs
+++ StudentsBlazorWebAssembly/Server/Migrations/DataContextModelSnapshot.cs
@@ -39,7 +39,7 @@ namespace StudentsBlazorWebAssembly.Server.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Students");
+                    b.ToTable("Students", (string)null);
                 });
 #pragma warning restore 612, 618
         }
